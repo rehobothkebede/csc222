4b6e5cee370592ce00eae19c50b91b71530365a8
rehobothkebede <rehobothkebede@gmail.com>
2024-11-19 08:26:05 -0500

    TEST

--- chapter10/num_vector_utils.cpp
+++ chapter10/num_vector_utils.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 string render_num_vector(const vector<int>& vec) {
 	string s = "";
-	if(vec.size()==0){
+	if(vec.size() == 0) {
 		return s;
 	} 
-	for(int i = 0; i<vec.size(); i++){
+	for(int i = 0; i < vec.size(); i++){
 		s+= to_string(vec[i]) + " ";
 	}
 	s.erase(s.length()-1,1);
--- chapter11/Time.cpp
+++ chapter11/Time.cpp
@@ -2,3 +2,16 @@
 #include <vector>
 #include <algortim>
 using namespace std;
+
+class Time{
+
+	struct Time{
+		int hour, minute;
+		double second;
+	}
+
+	void to_string(){
+		
+	}
+
+}
