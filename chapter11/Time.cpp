07bd9447667cf48902878396a5970c32a8ee0d28
rehobothkebede <rehobothkebede@gmail.com>
2024-11-20 12:32:06 -0500

    Test

--- chapter11/Time.cpp
+++ chapter11/Time.cpp
@@ -1,17 +1,25 @@
 #include <string>
 #include <vector>
 #include <algortim>
+#include "Time.h"
 using namespace std;
 
-class Time{
+Time: :Time(int sec){
+	int min = 0;
+	int hr = 0;
+	this->sec = sec;
+	
+}
 
-	struct Time{
-		int hour, minute;
-		double second;
-	}
+Time: :Time(int hr, int min){
+	this -> hr = hr;
+	this -> hr = hr;
 
-	void to_string(){
-		
-	}
+	
+}
+
+Time: :Time(int hr, int sec 
+void Time::to_string() const{
+	
 
 }
--- chapter11/Time.h
+++ chapter11/Time.h
@@ -1,4 +1,12 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include "Time.cpp"
 using namespace std;
+
+struct Time {
+	int sec, min, hr;
+	Time(sec);
+	
+	void to_string() const;
+}
