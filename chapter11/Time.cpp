10ca7073b26e8474d1544593ae9cfe3935c169d0
rehobothkebede <rehobothkebede@gmail.com>
2024-11-21 22:05:55 -0500

    Im too good

--- chapter11/Time.cpp
+++ chapter11/Time.cpp
@@ -16,10 +16,39 @@ Time::Time(int sec) : hr(0), min(0), sec(sec) {
 
 }
 
-Time::Time(int hr, int min) : hr(hr), min(min), sec(0) {}
+Time::Time(int hr, int min) : hr(hr), min(min), sec(0) {
+	if(this->min>=60){
+                this -> hr = min/60;
+                this -> min = min%60;
+        }
 
-Time::Time(int hr, int min, int sec) : hr(hr), min(min), sec(sec) {}
+}
+
+Time::Time(int hr, int min, int sec) : hr(hr), min(min), sec(sec) {
+	if(this->sec>60){
+                this -> min = sec/60;
+                this -> sec = sec%60;
+        }
+        if(this->min>=60){
+                this -> hr = min/60;
+                this -> min = min%60;
+        }
+
+}
 
 string Time::to_string() const {
     return std::to_string(hr) + ":" + (min < 10 ? "0" : "") + std::to_string(min) + ":" + (sec < 10 ? "0" : "") + std::to_string(sec);
 }
+
+
+Time Time::operator+(const Time& other) const {
+    int totalSec = this->sec + other.sec;
+    int totalMin = this->min + other.min + totalSec / 60;
+    int totalHr = this->hr + other.hr + totalMin / 60;
+
+    totalSec %= 60;
+    totalMin %= 60;
+    
+    return Time(totalHr, totalMin, totalSec);
+}
+
--- chapter11/test_times.cpp
+++ chapter11/test_times.cpp
@@ -13,3 +13,16 @@ TEST_CASE("Test can create and render Times") {
     Time t4(7 * 3600 + 11 * 60 + 19);
     CHECK(t4.to_string() == "7:11:19");
 }
+TEST_CASE("Test hour-minute and hour-minute-second constructors") {
+    Time t1(5, 37);
+    CHECK(t1.to_string() == "5:37:00");
+    Time t2(7, 2, 11);
+    CHECK(t2.to_string() == "7:02:11");
+}
+TEST_CASE("Test can add two Times with + operator") {
+    Time t1(25, 40);
+    Time t2(17, 2, 42);
+    Time t3 = t1 + t2;
+    CHECK(t3.to_string() == "42:42:42");
+}
+
