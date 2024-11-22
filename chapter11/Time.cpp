b1b0a2011801252f490f3c91e1f97a0deb71de38
rehobothkebede <rehobothkebede@gmail.com>
2024-11-21 21:32:33 -0500

    It compiles at least

--- chapter11/Time.cpp
+++ chapter11/Time.cpp
@@ -1,47 +1,15 @@
-#include <string>
-#include <vector>
-#include <algorithm>
 #include "Time.h"
 using namespace std;
 
-struct Time {
-	int hr, min, sec;
 
-	Time();	
-	Time(int);
-	Time(int,int);
-	Time(int,int,int);
-	string to_string() const;
+Time::Time() : hr(0), min(0), sec(0) {}
 
-Time(){
-	hr = 0;
-	min = 0;
-	sec = 0;
+Time::Time(int sec) : hr(0), min(0), sec(sec) {}
+
+Time::Time(int hr, int min) : hr(hr), min(min), sec(0) {}
+
+Time::Time(int hr, int min, int sec) : hr(hr), min(min), sec(sec) {}
+
+string Time::to_string() const {
+    return std::to_string(hr) + ":" + (min < 10 ? "0" : "") + std::to_string(min) + ":" + (sec < 10 ? "0" : "") + std::to_string(sec);
 }
-
-/*Time: :Time(int* sec){
-	min = 0;
-	hr = 0;
-	this->sec = sec;
-	
-}
-
-Time: :Time(int hr, int min){
-	this -> hr = hr;
-	this -> min = min;
-	sec = 0;
-	
-}
-
-Time: :Time(int hr, int min, int sec){
-	this -> hr = hr;
-	this -> min = min;
-	this -> sec = sec;
-
-}*/
-
-	string to_string() const{
-		return "";
-	}
-
-};
