0508bf4a21f3e95cd8a1191baeb0aacee892af06
rehobothkebede <rehobothkebede@gmail.com>
2024-12-11 19:14:57 -0500

    passing

--- chapter12/Fractions.cpp
+++ chapter12/Fractions.cpp
@@ -17,9 +17,7 @@ Fraction::Fraction(string s){
     for(int i = 0; i < s.length(); i++) {
        if(s[i] == '/') {
            this -> numerator = stoi(s.substr(0,i));
-       }
-       else {
-            this -> denominator = stoi(s.substr(i+1,s.length()-1));
+           this -> denominator = stoi(s.substr(i+1,s.length()-1));
        }
     }
 }
