8f74c17b5a05977d6954cb28c9ceb761106b97ce
rehobothkebede <rehobothkebede@gmail.com>
2024-12-18 11:55:37 -0500

    Passing test

--- chapter13/Cards.cpp
+++ chapter13/Cards.cpp
@@ -9,16 +9,16 @@ Card::Card(Suit suit,Rank rank){
     this->rank=rank;
 }
 Card::Card(){
-    this->suit = NONE;
-    this->rank = JACK;
+    this->rank = JOKER;
 }
 string Card::to_string() const{
-    vector<string> suit_strings = {"Clubs", "Diamonds", "Hearts", "Spades"};
+    vector<string> suit_strings = {"NONE", "Clubs", "Diamonds", "Hearts", "Spades"};
     vector<string> rank_strings = {"Joker", "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 
-    if (rank == 0){
-        return "None";
+    if (!rank){
+        return "Joker"; 
     }
+       
     
     return rank_strings[rank] + " of " + suit_strings[suit];
 }
--- chapter13/Cards.h
+++ chapter13/Cards.h
@@ -4,19 +4,18 @@
 
 using namespace std;
 
+
 struct Deck;
 
 enum Suit {NONE, CLUBS, DIAMONDS, HEARTS, SPADES};
-enum Rank {JOKER, TWO=2, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT,
-           NINE, TEN, JACK, QUEEN, KING, ACE};
+enum Rank {JOKER, TWO=2, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE};
 
 struct Card{
-
     Suit suit;
     Rank rank;
 
     string to_string() const;
     
-    Card(Suit, Rank);
     Card();
+    Card(Suit, Rank);
 };
